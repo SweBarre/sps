2445be1a5f1847d631e2d91b3fdf9feb69219d95
Jonas Forsberg <jonas.forsberg@suse.com>
2020-06-05 15:17:47 +0200

    Reformat line length

--- src/sps/cli.py
+++ src/sps/cli.py
@@ -49,7 +49,10 @@ def create_parser(args=sys.argv[1:]):
                 action="store_true",
             )
             parser.add_argument(
-                "--short", "-s", help="Just print product identifier", action="store_true",
+                "--short",
+                "-s",
+                help="Just print product identifier",
+                action="store_true",
             )
         if args[0] == "package":
             parser.add_argument("command", help="package related tasks")
